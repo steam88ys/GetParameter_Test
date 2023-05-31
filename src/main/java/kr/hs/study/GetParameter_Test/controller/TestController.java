9e38a0c5c989efe24db99d6a46391c1edb5463b6
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-06-01 08:51:08 +0900

    1. HttpServletRequest 2. WebRequest 3. @RequestParam 4. @ModelAttribute 4가지 방법으로 회원가입 기능 추가

--- src/main/java/kr/hs/study/GetParameter_Test/controller/TestController.java
+++ src/main/java/kr/hs/study/GetParameter_Test/controller/TestController.java
@@ -1,11 +1,15 @@
 package kr.hs.study.GetParameter_Test.controller;
 
+import kr.hs.study.GetParameter_Test.dto.join;
 import kr.hs.study.GetParameter_Test.dto.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TestController {
@@ -44,6 +48,76 @@ public class TestController {
     public String re_login(@ModelAttribute login log) {
         // login객체 log를 만들고 사용자의 입력값을 멤버변수 setMethod 이용해서 자동주입
         System.out.println("id: " + log.getUser_id() + ", pass: " + log.getUser_pass());
+        return "result";
+    }
+
+    @GetMapping("/joinUs")
+    public String join() {
+        return "join";
+    }
+
+    // HttpServletRequet
+//    @PostMapping("/join")
+//    public String join(HttpServletRequest req) {
+//        String id = req.getParameter("id");
+//        String pass = req.getParameter("pass");
+//        String name = req.getParameter("name");
+//        String email = req.getParameter("email");
+//        String tel = req.getParameter("tel");
+//        String local = req.getParameter("local");
+//        System.out.println("id: "+id);
+//        System.out.println("pass: "+pass);
+//        System.out.println("name: "+name);
+//        System.out.println("email: "+email);
+//        System.out.println("tel: "+tel);
+//        System.out.println("local: "+local);
+//        return "result";
+//    }
+
+    // WebRequest
+//    @PostMapping("/join")
+//    public String join(WebRequest req) {
+//        String id = req.getParameter("id");
+//        String pass = req.getParameter("pass");
+//        String name = req.getParameter("name");
+//        String email = req.getParameter("email");
+//        String tel = req.getParameter("tel");
+//        String local = req.getParameter("local");
+//        System.out.println("id: "+id);
+//        System.out.println("pass: "+pass);
+//        System.out.println("name: "+name);
+//        System.out.println("email: "+email);
+//        System.out.println("tel: "+tel);
+//        System.out.println("local: "+local);
+//        return "result";
+//    }
+
+    // @RequestParam
+//    @PostMapping("/join")
+//    public String join(@RequestParam("id") String id,
+//                       @RequestParam("pass") String pass,
+//                       @RequestParam("name") String name,
+//                       @RequestParam("email") String email,
+//                       @RequestParam("tel") String tel,
+//                       @RequestParam("local") String local) {
+//        System.out.println("id: "+id);
+//        System.out.println("pass: "+pass);
+//        System.out.println("name: "+name);
+//        System.out.println("email: "+email);
+//        System.out.println("tel: "+tel);
+//        System.out.println("local: "+local);
+//        return "result";
+//    }
+
+    // @ModelAttribute
+    @PostMapping("/join")
+    public String join(@ModelAttribute join t ){
+        System.out.println("id: "+t.getId());
+        System.out.println("pass: "+t.getPass());
+        System.out.println("name: "+t.getName());
+        System.out.println("email: "+t.getEmail());
+        System.out.println("tel: "+t.getTel());
+        System.out.println("local: "+t.getLocal());
         return "result";
     }
 
---
+++ src/main/java/kr/hs/study/GetParameter_Test/dto/join.java
@@ -0,0 +1,58 @@
+package kr.hs.study.GetParameter_Test.dto;
+
+public class join {
+    private String id;
+    private String pass;
+    private String name;
+    private String email;
+    private String tel;
+    private String local;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+}
