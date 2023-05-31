269cc6d3b7b33d8cc3afb41728a0bddfb8a47f35
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-05-31 11:17:00 +0900

    ModelAttribute 사용

--- src/main/java/kr/hs/study/GetParameter_Test/controller/TestController.java
+++ src/main/java/kr/hs/study/GetParameter_Test/controller/TestController.java
@@ -1,12 +1,11 @@
-package kr.hs.study.GetParameter_Test.conrtroller;
+package kr.hs.study.GetParameter_Test.controller;
 
+import kr.hs.study.GetParameter_Test.dto.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.WebRequest;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class TestController {
@@ -32,10 +31,20 @@ public class TestController {
 //        return "result";
 //    }
 
+//    @PostMapping("/re_login")
+//    public String re_login(@RequestParam("user_id") String id,
+//                           @RequestParam("user_pass") String pass) {
+//        System.out.println("id: " + id + ", pass: " + pass);
+//        return "result";
+//    }
+
+    // @ModelAttribut로 받는다. dto가 있어야 함
+    // dto ==> 자바파일
     @PostMapping("/re_login")
-    public String re_login(@RequestParam("user_id") String id,
-                           @RequestParam("user_pass") String pass) {
-        System.out.println("id: " + id + ", pass: " + pass);
+    public String re_login(@ModelAttribute login log) {
+        // login객체 log를 만들고 사용자의 입력값을 멤버변수 setMethod 이용해서 자동주입
+        System.out.println("id: " + log.getUser_id() + ", pass: " + log.getUser_pass());
         return "result";
     }
+
 }
